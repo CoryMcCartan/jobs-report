e8fe4caa77b2b34df2a19619b5299c790da2d3b4
Cory McCartan <cory.mccartan@gmail.com>
2021-05-10 00:31:00 -0400

    gh actions (1)

--- run.R
+++ run.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
-
+if (!requireNamespace("here", quietly=T)) install.packages("here")
+if (!requireNamespace("targets", quietly=T)) install.packages("targets")
+if (!requireNamespace("stringr", quietly=T)) install.packages("stringr")
 
 # For rendering to website
 render_site_proj = function(input, ..., site_path="~/Documents/Code/website/content/projects/") {
@@ -18,15 +20,32 @@ render_site_proj = function(input, ..., site_path="~/Documents/Code/website/cont
 }
 
 args = commandArgs(trailingOnly=TRUE)
-print_usage = function() cat("Usage: run.R [install|make|render|help]\n")
+print_usage = function() cat("Usage: run.R [deps|install|make|render|help]\n")
+dep_file = here::here(".github/depends.txt")
 if (length(args) == 0 || args[1] == "help") {
     print_usage()
-} else if (args[1] == "install") {
-    cat("INSTALLING REQUIRED PACKAGES\n")
+} else if (args[1] == "deps") {
     library(stringr)
     tmp = tempfile()
     targets::tar_renv(extras=character(0), path=tmp)
     packages = na.omit(str_extract(readLines(tmp), "(?<=library\\()[a-zA-Z-]+"))
+    packages = c(packages, "rmarkdown")
+    if (dir.exists(here::here(".github"))) {
+        writeLines(packages, dep_file)
+        writeLines(sprintf("R-%i.%i", getRversion()$major, getRversion()$minor),
+                   here::here(".github/R-version"))
+    }
+} else if (args[1] == "install") {
+    cat("INSTALLING REQUIRED PACKAGES\n")
+    library(stringr)
+    if (!file.exists(dep_file)) {
+        tmp = tempfile()
+        targets::tar_renv(extras=character(0), path=tmp)
+        packages = na.omit(str_extract(readLines(tmp), "(?<=library\\()[a-zA-Z-]+"))
+        packages = c(packages, "rmarkdown")
+    } else {
+        packages = readLines(dep_file)
+    }
     to_install = setdiff(packages, rownames(installed.packages()))
     if (length(to_install) > 0)
         install.packages(to_install)
